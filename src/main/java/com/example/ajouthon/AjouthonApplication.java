8c300fdfe9a12a57687e66d0ffdd830eb2bfd6c7
JisinKeo <tntjd9514@naver.com>
2023-03-19 00:00:28 +0900

    initial

---
+++ src/main/java/com/example/ajouthon/AjouthonApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajouthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjouthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjouthonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ajouthon/AjouthonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajouthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjouthonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
